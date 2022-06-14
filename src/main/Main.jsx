a3745a0d8efa53e0fe036f7a7aa91e034fb4d6cc
James <jamespatterson1985@yahoo.com>
2022-06-14 02:57:25 -0400

    movie-db: mobile(build): extract reusable components.

--- src/main/Main.jsx
+++ src/main/Main.jsx
@@ -1,24 +1,12 @@
+import React from 'react';
+import {Movie} from '../movie/movie';
 import './main.css';
 
-const movie = { "Poster": "N/A", "Title": "Spiderman", "Type": "movie", "Year": "2010", "imdbID": "tt1785572"};
-
-const {Poster, Title, Type, Year, imdbID, NotFound} = {...movie, NotFound: 'https://via.placeholder.com/400'};
-
-const IMG_STYLES = { width: "auto", maxWidth: "100%", height: "auto", borderRadius: "2vw" };
-const H5_STYLES = { textAlign: "center"};
 
 export default function Main() {
     return (
-        <div className="main">
-            <div className="card text-white bg-secondary" style={IMG_STYLES}>
-                <div className="card-body">
-                    <h5 className="card-title" style={H5_STYLES}>{Title} ({Year})</h5>
-                </div>
-                <img className="card-img-top" src={Poster === 'N/A' ? NotFound : Poster} alt={Title}></img>
-                <div className="card-body">
-                    <h5 className="card-title" style={H5_STYLES}>{Type}</h5>
-                </div>
-            </div>
-        </div>
+        <React.Fragment>
+            <Movie/>
+        </React.Fragment>
     );
 }
---
+++ src/spacer/spacer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SPACER = {"height": "2vh", "width": "100%"};
+
+export const Spacer = () => {
+    return (
+        <div style={SPACER}></div>
+    );
+}
