7b3090256843129a68a3e2d926877547f61a7349
James <jamespatterson1985@yahoo.com>
2022-06-18 13:32:46 -0400

    movie-database: build(http.jsx): attempting to get HTTP service to return values

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Http from '../../rest/Http';
+import HttpService from '../../rest/Http.service';
 import Movies from '../movies/movies';
 import MOVIE_LIST from '../../static/movies.json';
 import './main.css';
@@ -7,16 +8,18 @@ import './main.css';
 const MOVIE = { "Poster": "N/A", "Type": "movie", "imdbID": "" };
 const movies = [];
 
+
 export default function Main() {
+    console.log(HttpService({request: {url: 'https://jsonplaceholder.typicode.com/users', method: 'GET'}}));
     for (let i = 0, iLen = MOVIE_LIST.length; i < iLen; i++) {
         const title = MOVIE_LIST[i].slice(0, -5).replace('\"', '');
         const year = title.includes('(') && title.includes(')') ? title.slice(title.indexOf('(')) : '';
         movies.push({...MOVIE, Title: title.replace(' ' + year, ''), NotFound: 'https://via.placeholder.com/400', Year: year });
     }
-
+    
     return (
         <React.Fragment>
-            <Http request={{url: 'https://jsonplaceholder.typicode.com/users', method: 'GET'}} />
+            {/* <Http request={{url: 'https://jsonplaceholder.typicode.com/users', method: 'GET'}} /> */}
             <Movies movies={movies}/>
         </React.Fragment>
     );
--- src/rest/Http.service.jsx
+++ src/rest/Http.service.jsx
@@ -1,17 +1,16 @@
-export default function HTTP(props) {
-    Query(props);
-}
-
-function Query(props) {
+export default function HttpService(props) {
     /** @todo Implement spinner component */
     if (!props.request.url || !props.request.method) {
         return;
     }
     const query = async (props, req) => {
         const _request = props.request.headers ? { ...req, headers: props.request.headers } : req;
-        await fetch(props.request.url, props.request.body ? { ..._request, body: JSON.stringify(props.request.body)} : _request)
+        const _response = await fetch(props.request.url, props.request.body ? { ..._request, body: JSON.stringify(props.request.body)} : _request)
             .then(response => response.json())
-            // .then(data => console.log(data))
+            .then(data => {
+                console.log('Received: ', data);
+                return data;
+            })
             .catch(e => { return; });
     }
     query(props, { method: props.request.method });
