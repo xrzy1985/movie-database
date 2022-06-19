7f1c1c43e0b2693d2d8bbbfa6c14c521d5331b59
James <jamespatterson1985@yahoo.com>
2022-06-19 01:48:50 -0400

    test

--- src/components/movies/movies.jsx
+++ src/components/movies/movies.jsx
@@ -3,5 +3,6 @@ import { Movie } from '../movie/movie';
 import { MovieNotFound } from '../movie/movieNotFound';
 
 export default function Movies(props) {
-    return props.movies?.length ? props.movies.map((m, index) => { return <Movie key={index} movie={m}/> }) : <MovieNotFound />;
+    return props.movies?.length ?
+        props.movies.map((m, index) => { return <Movie key={index} movie={m}/> }) : <MovieNotFound />;
 }
